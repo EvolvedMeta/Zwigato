27332790939f6b77082d550f0d281eef5e17ec15
EvolvedMeta <yashbindal815@gmail.com>
2024-02-24 22:32:42 +0530

    chore: initial setup

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const HeadingComponent = () => { return <h1>Initial Setup</h1> };
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
+
