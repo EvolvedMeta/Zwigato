8064ed3ad0accae0326c06e475a839c69cf253d3
EvolvedMeta <yashbindal815@gmail.com>
2024-03-03 01:30:05 +0530

    chore: add mock data

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,30 +1,24 @@
 import { ResCard } from "./ResCard.js";
 import "../styles/style.css";
+import { menuOptions } from "../../demo/mocks/menu_mock.js";
 export const Body = () => {
-    return(
+  return (
     <div className="body">
-        <div className="search-bar">
-            Search 
-        </div>
-        <div className="res-containter">
-            <ResCard />
-            <ResCard />
-            <ResCard />
-            <ResCard />
-            <ResCard />
-            <ResCard />
-            <ResCard />
-            <ResCard />
-            <ResCard />
-            <ResCard />
-            <ResCard />
-            <ResCard />
-            <ResCard />
-            <ResCard />
-            <ResCard />
-            <ResCard />
-            <ResCard />
-            <ResCard />
-        </div>
+      <div className="search-bar">Search</div>
+      <div className="res-containter">
+        {menuOptions.map((item) => (
+          <ResCard
+            key={item?.data?.id}
+            cloudinaryImageId={item.data?.cloudinaryImageId}
+            name = {item.data.name}
+            totalRatingsString={item?.data?.totalRatingsString}
+            cuisines={item?.data?.cuisines}
+            costForTwo={item?.data?.costForTwo}
+            costForTwoString={item?.data?.costForTwoString}
+            deliveryTime={item?.data?.deliveryTime}
+          />
+        ))}
+      </div>
     </div>
-)};
+  );
+};
--- src/component/ResCard.js
+++ src/component/ResCard.js
@@ -1,13 +1,17 @@
 import '../styles/style.css';
-export const ResCard = () => {
+export const ResCard = (props) => {
+  const { name, totalRatingsString, cuisines, costForTwo, costForTwoString, deliveryTime, cloudinaryImageId } = props;
   return (
-    
-    <div className="res-card">
-        <img className="res-image" alt="res-logo" src='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/lyzsowbcnuieb6i1cusc'/>
-        <h3 className="res-title">Dominos</h3>
-        <h4>Pizza, Garlic Bread, Desert</h4>
-        <h4>4.4 stars</h4>
-        <h4>39 minutes</h4>
+    <div className="res-card key">
+      <img className="res-image" alt="res-logo" src={
+        `https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_420,c_fill/`+ cloudinaryImageId
+      } />
+      <h3 className="res-title">{name}</h3>
+      <h4 className='res-item'>{cuisines.join(', ')}</h4>
+      <h4 className='res-item'>{totalRatingsString}</h4>
+      <h4 className='res-item'>Delivery in: {deliveryTime} mins</h4>
+      <h4 className='res-item'>{costForTwoString}</h4>
     </div>
+
   );
 };
